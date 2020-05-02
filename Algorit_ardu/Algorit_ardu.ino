9d00b21c85feb018e377c7d621af88b4909d1d15
Juan Ca <juancamilorg64@gmail.com>
2020-05-02 16:30:12 -0500

    ver 0.1.2

--- Algorit_ardu/Algorit_ardu.ino
+++ Algorit_ardu/Algorit_ardu.ino
@@ -1,3 +1,5 @@
+#include <TimerOne.h>
+
 // Variables para datos inmediato de valor de luz en logitudes de onda
 int L1_der;
 int L2_der;
@@ -18,13 +20,30 @@ int maxi[4];//Este vector guarda el valor maximo actual de todas las loguitudes 
 //Posicion 2 = max(L1_izq)
 //Posicion 3 = max(L2_izq)
 
-char datar=0; //Dato instantaneo leido
+// Valores de amplitud AC y DC
+//AC
+int L1AC_der;
+int L2AC_der;
+int L1AC_der;
+int L2AC_der;
+// DC
+int L1DC_der;
+int L2DC_der;
+int L1DC_izq;
+int L2DC_izq;
+
+
+
+char datar[]={'0','0','0','0'}; //Dato instantaneo leido
 int orden=0;    // orden para asignar datos a variables
 bool datnew=false; // llego nuevo dato
+//logica para guardar datos
+ int pos=0;
  
 void setup() {
   Serial.begin(9600); //iniciar comunicacion serial 
-  
+  Timer1.initialize(1000000);
+  Timer1.attachInterrupt(Envio);
 //-----------------------------------------------------------
  for (int i=0;i < 4; i++){ // Resetear minimos y maximos
   mini[i]=0;
@@ -33,7 +52,66 @@ void setup() {
 }
 
 void loop() {
-// Encontrar Maximo y minimo L1 der.-----
+cal_min_max();
+UsartRx();
+}
+
+
+void UsartRx() {
+ if (Serial.available() > 0) {
+ char dataexclude=(char)Serial.read();
+ if (dataexclude == 10 || dataexclude == ','){}
+ else{
+ datar[pos]=dataexclude;
+ pos++;
+  }
+ }
+ 
+
+ if(pos==4 || datnew==true) {
+  pos=0;datnew=false;
+ if(orden==0){ // Se lee primer dato L1_der
+   L1_der = atoi(&datar[0]);
+   orden=1;
+  }
+  else if(orden==1){
+   L2_der = atoi(&datar[0]);
+   orden=2;
+  }
+   else if(orden==2){
+   L1_izq = atoi(&datar[0]);
+   orden=3;
+  }
+  else if(orden==3){
+   L2_izq = atoi(&datar[0]);
+   orden=0;
+  }
+}
+}
+
+void Envio() {
+  //calculo AmpAC
+  L1
+  
+  //-------------__________  Enviar Datos ____________-----------------------------
+    Serial.print("Min ");
+    for (int i=0;i < 4; i++){ // Resetear minimos y maximos
+    Serial.print(mini[i]);
+    Serial.print(',');
+    }
+    Serial.println("");
+    Serial.print("Max ");
+    for (int i=0;i < 4; i++){ // Resetear minimos y maximos
+    Serial.print(maxi[i]);
+    Serial.print(',');
+    }
+    Serial.println("");
+    Serial.println(orden); //
+    Serial.println(atoi(&datar[0]));
+  }
+  
+void cal_min_max(){
+ // Encontrar Maximo y minimo L1 der.-----
   if (L1_der < mini[0]) {
     mini[0]=L1_der;
   }
@@ -61,49 +139,5 @@ void loop() {
   if (L2_izq > maxi[3]) {
     maxi[3]=L2_izq;
     }
-//-------------__________  Enviar Datos ____________-----------------------------
- 
- Serial.print("Min ");
- for (int i=0;i < 4; i++){ // Resetear minimos y maximos
- Serial.print(mini[i]);
- Serial.print(',');
+//-------------------------------------
   }
- Serial.println("");
- Serial.print("Max ");
-for (int i=0;i < 4; i++){ // Resetear minimos y maximos
- Serial.print(maxi[i]);
- Serial.print(',');
-  }
-Serial.println("");
-Serial.println(orden); //
-
-delay(1000);
-UsartRx();
-}
-
-
-void UsartRx() {
- if (Serial.available() > 0) {
- datar=(char)Serial.read();
- datnew=true;
- }
- if(datnew==true) {
-  datnew=false;
- if(orden==0){ // Se lee primer dato L1_der
-   L1_der = (int)datar;
-   orden=1;
-  }
-  else if(orden==1){
-   L2_der = (int)datar;
-   orden=2;
-  }
-   else if(orden==2){
-   L1_izq = (int)datar;
-   orden=3;
-  }
-  else if(orden==3){
-   L2_izq = (int)datar;
-   orden=0;
-  }
-}
-}//Usart
